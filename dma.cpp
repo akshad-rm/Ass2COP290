f250d199f74d4840342375f3f45c673dde713d01
Vraj Parikh <137074800+vparikh3001@users.noreply.github.com>
2024-02-12 19:56:20 +0530

    Update dma.cpp

--- dma.cpp
+++ dma.cpp
@@ -122,7 +122,7 @@ void solve(int n,int x,int p,vector<pair<string,db>>&data,vector<pair<string,str
 
 void write_data(vector<pair<string,string>>&daily_cashflow,vector<vector<string>>&order_stats){
     
-    ofstream dc_file("daily cashflow.csv");
+    ofstream dc_file("daily_cashflow.csv");
     dc_file<<"Date,Cashflow"<<endl;
     for(auto x:daily_cashflow){
         dc_file<<x.first<<","<<x.second<<endl;
