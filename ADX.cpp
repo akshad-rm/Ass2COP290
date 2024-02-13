bfaba473247f8c5b3fce536fff15f2863424fe1d
Vraj Parikh <137074800+vparikh3001@users.noreply.github.com>
2024-02-13 22:46:31 +0530

    Update ADX.cpp

--- ADX.cpp
+++ ADX.cpp
@@ -237,7 +237,7 @@ void solve(int x,int n, db adx_threshold,string start_date,string end_date,vecto
         hold_quantity = 0;
     }
     ofstream res_file("final_pnl.txt");
-    res_file<<cash_in_hand<<endl;
+    res_file<<to_string(cash_in_hand)<<endl;
     res_file.close();
     
     
