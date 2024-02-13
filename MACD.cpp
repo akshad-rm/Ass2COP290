34cb0273bdd61888989a1e4b9f2d95bfc0025779
Vraj Parikh <137074800+vparikh3001@users.noreply.github.com>
2024-02-13 15:48:45 +0530

    Update MACD.cpp

--- MACD.cpp
+++ MACD.cpp
@@ -196,7 +196,7 @@ void solve(int x,vector<pair<string,db>>&data,vector<pair<string,string>>&daily_
         hold_quantity = 0;
     }
     ofstream res_file("final_pnl.txt");
-    res_file<<cash_in_hand<<endl;
+    res_file<<to_string(cash_in_hand)<<endl;
     res_file.close();
     
 }
